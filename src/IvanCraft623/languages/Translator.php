1229757fa577ecd1ec618ab4f5b49d11859b78ae
xxAROX <arox.email.0@gmail.com>
2023-05-09 11:28:08 +0200

    Update Translator.php
    Sync all locales from https://github.com/Mojang/bedrock-samples/blob/main/resource_pack/texts/language_names.json

--- src/IvanCraft623/languages/Translator.php
+++ src/IvanCraft623/languages/Translator.php
@@ -15,117 +15,36 @@ final class Translator {
 	public static array $translators = [];
 
 	public const MINECRAFT_LOCALES = [
-		# https://minecraft.fandom.com/wiki/Language
-	    "af_ZA", // Afrikaans
-	    "ar_SA", // Arabic
-	    "ast_ES", // Asturian
-		"az_AZ", // Azerbaijani
-		"bg_BG", // Bulgarian
-		"bn_BD", // Bengali
-		"bs_BA", // Bosnian
-		"ca_ES", // Catalan
-		"cs_CZ", // Czech
-		"cy_GB", // Welsh
-		"da_DK", // Danish
-		"de_DE", // German
-		"el_GR", // Greek
-		"en_AU", // English, Australian
-		"en_CA", // English, Canadian
-		"en_GB", // English, British
-		"en_NZ", // English, New Zealand
-		"en_PT", // English, Pirate
-		"en_UD", // English, upside down
-		"en_US", // English, US
-		"eo_UY", // Esperanto
-		"es_AR", // Spanish, Argentine
-		"es_CL", // Spanish, Chilean
-		"es_ES", // Spanish, Spain
-		"es_MX", // Spanish, Mexican
-		"et_EE", // Estonian
-		"eu_ES", // Basque
-		"fa_IR", // Farsi
-		"fi_FI", // Finnish
-		"fil_PH", // Filipino
-		"fo_FO", // Faroese
-		"fr_CA", // French, Canadian
-		"fr_FR", // French, France
-		"fy_NL", // Frisian
-		"ga_IE", // Irish
-		"gd_GB", // Scottish Gaelic
-		"gl_ES", // Galician
-		"gu_IN", // Gujarati
-		"he_IL", // Hebrew
-		"hi_IN", // Hindi
-		"hr_HR", // Croatian
-		"hu_HU", // Hungarian
-		"hy_AM", // Armenian
-		"id_ID", // Indonesian
-		"ig_NG", // Igbo
-		"io_EN", // Ido
-		"is_IS", // Icelandic
-		"it_IT", // Italian
-		"ja_JP", // Japanese
-		"jv_ID", // Javanese
-		"ka_GE", // Georgian
-		"kk_KZ", // Kazakh
-		"km_KH", // Khmer
-		"kn_IN", // Kannada
-		"ko_KR", // Korean
-		"ku_TR", // Kurdish
-		"la_LA", // Latin
-		"lb_LU", // Luxembourgish
-		"lo_LA", // Lao
-		"lt_LT", // Lithuanian
-		"lv_LV", // Latvian
-		"mg_MG", // Malagasy
-		"mi_NZ", // Maori
-		"mk_MK", // Macedonian
-		"ml_IN", // Malayalam
-		"mn_MN", // Mongolian
-		"mr_IN", // Marathi
-		"ms_MY", // Malay
-		"mt_MT", // Maltese
-		"nb_NO", // Norwegian Bokmål
-		"ne_NP", // Nepali
-		"nl_NL", // Dutch
-		"nn_NO", // Norwegian Nynorsk
-		"no_NO", // Norwegian
-		"nso_ZA", // Northern Sotho
-		"oc_FR", // Occitan
-		"or_IN", // Oriya
-		"pa_IN", // Punjabi
-		"pl_PL", // Polish
-		"pt_BR", // Portuguese, Brazil
-		"pt_PT", // Portuguese, Portugal
-		"qu_PE", // Quechua
-		"ro_RO", // Romanian
-		"ru_RU", // Russian
-		"sc_IT", // Sardinian
-		"se_NO", // Northern Sami
-		"sk_SK", // Slovak
-		"sl_SI", // Slovenian
-		"sq_AL", // Albanian
-		"sr_RS", // Serbian
-		"sv_SE", // Swedish
-		"sw_KE", // Swahili
-		"ta_IN", // Tamil
-		"te_IN", // Telugu
-		"th_TH", // Thai
-		"tl_PH", // Tagalog
-		"tr_TR", // Turkish
-		"tt_RU", // Tatar
-		"udm_RU", // Udmurt
-		"uk_UA", // Ukrainian
-		"ur_PK", // Urdu
-		"uz_UZ", // Uzbek
-		"vi_VN", // Vietnamese
-		"xh_ZA", // Xhosa
-		"yi_DE", // Yiddish
-		"yo_NG", // Yoruba
-		"zh_CN", // Chinese, Simplified
-		"zh_HK", // Chinese, Hong Kong
-		"zh_TW", // Chinese, Traditional
-		"zu_ZA", // Zulu
+		// See  ->  https://github.com/Mojang/bedrock-samples/blob/main/resource_pack/texts/language_names.json
+		"en_US", // English (United States)
+		"en_GB", // English (United Kingdom)
+		"de_DE", // Deutsch (Deutschland)
+		"es_ES", // Español (España)
+		"es_MX", // Español (México)
+		"fr_FR", // Français (France)
+		"fr_CA", // Français (Canada)
+		"it_IT", // Italiano (Italia)
+		"ja_JP", // 日本語 (日本)
+		"ko_KR", // 한국어 (대한민국)
+		"pt_BR", // Português (Brasil)
+		"pt_PT", // Português (Portugal)
+		"ru_RU", // Русский (Россия)
+		"zh_CN", // 中文(简体)
+		"zh_TW", // 中文(繁體)
+		"nl_NL", // Nederlands (Nederland)
+		"bg_BG", // Български (България)
+		"cs_CZ", // Čeština (Česko)
+		"da_DK", // Dansk (Danmark)
+		"el_GR", // Ελληνικά (Ελλάδα)
+		"fi_FI", // Suomi (Suomi)
+		"hu_HU", // Magyar (Magyarország)
+		"id_ID", // Indonesia (Indonesia)
+		"nb_NO", // Norsk bokmål (Norge)
+		"pl_PL", // Polski (Polska)
+		"sk_SK", // Slovenčina (Slovensko)
+		"sv_SE", // Svenska (Sverige)
+		"tr_TR", // Türkçe (Türkiye)
+		"uk_UA", // Українська (Україна)
 	];
 
 	private PluginBase $plugin;
